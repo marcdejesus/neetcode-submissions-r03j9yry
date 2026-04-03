85c24b2c613bedea866b438276a1cbc9ffb534d7
Marc De Jesus <131229911+marcdejesus@users.noreply.github.com>
2026-04-03 18:45:39 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,73 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        # Skip when string lengths aren't equal
+        if len(s) != len(t):
+            return False
+
+        # Alphabet hashmap to store char counts
+        count_s = {
+            'a': 0,
+            'b': 0,
+            'c': 0,
+            'd': 0,
+            'e': 0,
+            'f': 0,
+            'g': 0,
+            'h': 0,
+            'i': 0,
+            'j': 0,
+            'k': 0,
+            'l': 0,
+            'm': 0,
+            'n': 0,
+            'o': 0,
+            'p': 0,
+            'q': 0,
+            'r': 0,
+            's': 0,
+            't': 0,
+            'u': 0,
+            'v': 0,
+            'w': 0,
+            'x': 0,
+            'y': 0,
+            'z': 0
+        }
+
+        count_t = {
+            'a': 0,
+            'b': 0,
+            'c': 0,
+            'd': 0,
+            'e': 0,
+            'f': 0,
+            'g': 0,
+            'h': 0,
+            'i': 0,
+            'j': 0,
+            'k': 0,
+            'l': 0,
+            'm': 0,
+            'n': 0,
+            'o': 0,
+            'p': 0,
+            'q': 0,
+            'r': 0,
+            's': 0,
+            't': 0,
+            'u': 0,
+            'v': 0,
+            'w': 0,
+            'x': 0,
+            'y': 0,
+            'z': 0
+        }
+
+        for char in s:
+            count_s[char] += 1
+        
+        for char in t: 
+            count_t[char] += 1
+        
+        return count_t == count_s
